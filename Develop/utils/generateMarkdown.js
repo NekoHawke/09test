aebcbf6b3b7faa515561d1427ebaeee5381cc294
callahn mcdonald <callahn@gmail.com>
2022-10-06 17:31:26 -0600

    cleaning

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,61 +1,39 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license === "NONE"){
-    return ""
-  } else {
-    return `![license](https://img.shields.io/badge/License-${license}-brightgreen.svg)`
-}
-}
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  ## Licensing:
+  [![license](https://img.shields.io/badge/license-${data.licensing}-blue)](https://shields.io)
 
-// TODO: Create a function to generate markdown for README
-const generateMarkdown = (readmeInfo) => {
-  return `
-  ## ${readmeInfo.title}
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Additional Info](#additional-info)
 
-  ### Created by: ${readmeInfo.name}
-  <a href="https://github.com/${readmeInfo.name}">Github: ${readmeInfo.name} </a>
+  ## Description:
+  ${data.description}
 
-  ## Table Of Contents
-  
-  *[Project Description](#project-description)
-  *[License Information](#license-information)
-  *[Installion Instructions](#installion-instruction)
-  *[Usage Information](#usage-information)
-  *[Project Contribution](#project-contributions)
-  *[Testing Instructions](#testing-instructions)
-  *[Contact](#contact)
-  
+  ## Installation:
+  ${data.installation}
 
-  ## Project Description
-  ${readmeInfo.description}
+  ## Usage:
+  ${data.usage}
 
-  ## License Information
-  ${renderLicenseBadge(readmeInfo.license)}
+  ## License:
+  ${data.licensing}
 
-  ## Installion Instructions
-  ${readmeInfo.instructions}
+  ## Contribution:
+  ${data.contribution}
 
-  ## Usage Information
-  ${readmeInfo.information}
+  ## Testing:
+  ${data.testing}
 
-  ## Project Contributions
-  ${readmeInfo.guidelines}
-
-  ## Testing Instructions
-  ${readmeInfo.testing}
-
-  ## Contact 
-  ${readmeInfo.email}
-
- `;
+  ## Additional Info:
+  - Github: [${data.github}](https://github.com/${data.github})
+  - Email: ${data.email} `;
 }
 
 module.exports = generateMarkdown;
