4020d49baf233b35c47b92e9394a6b608a5289da
callahn mcdonald <callahn@gmail.com>
2022-11-07 14:56:01 -0700

    last minute struggles

--- Develop/index.js
+++ Develop/index.js
@@ -1,166 +1,75 @@
-// TODO: Include packages needed for this application
-const inquirer = require("inquirer");
-const fs = require('fs');
-const generateMarkdown = require("./utils/generateMarkdown");
-
-// TODO: Create an array of questions for user input
-const questions = () => {
-    return inquirer.prompt ([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'Please enter your project title? (Required)',
-            validate: titleInput => {
-                if(titleInput) {
-                    return true
-                } else {
-                    console.log('Please enter your project title!')
-                }
-            }
-        },
-        {
-            type: 'confirm',
-            name: 'confirmDescription',
-            message: 'Would you like to add a description about your project?',
-            default: true 
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Provide some information about your project:',
-            when: ({ confirmDescription }) => {
-                if (confirmDescription) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your Github username? (Required)',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your Github username!')
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email address? (Required)',
-            validate: emailInput => {
-                if (emailInput) {
-                    return true;
-                } else {
-                    console.log('Enter your email');
-                    return false
-                }
-            }
-        },
-        {
-            type: 'confirm',
-            name: 'installInstruction',
-            message: 'Is there any installation instructions you would like us to know about your project?',
-            default: true 
-        },
-        {
-            type: 'input',
-            name: 'instructions',
-            message: 'Please provide the installation instructions about your project:',
-            when: ({ installInstruction }) => {
-                if (installInstruction) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'confirm',
-            name: 'usageInformation',
-            message: 'Is there any Usage Information you would like us to know about your project?',
-            default: true, 
-        },
-        {
-            type: 'input',
-            name: 'information',
-            message: 'Please provide the Usage Information about your project:',
-            when: ({ usageInformation }) => {
-                if (usageInformation) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'confirm',
-            name: 'contributionGuidelines',
-            message: 'Is there any Contribution Guidelines you would like us to know about your project?',
-            default: true, 
-        },
-        {
-            type: 'input',
-            name: 'guidelines',
-            message: 'Please provide the details of all the contributions to your project:',
-            when: ({ contributionGuidelines }) => {
-                if (contributionGuidelines) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'confirm',
-            name: 'testInstructions',
-            message: 'Are there any Test Instructions you would like us to know about your project?',
-            default: true, 
-        },
-        {
-            type: 'input',
-            name: 'testing',
-            message: 'Please provide the Test Instructions for your project:',
-            when: ({ testInstructions }) => {
-                if (testInstructions) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'checkbox' ,
-            name: 'license',
-            message: 'What License did you use in this project? (Check the box that applys)',
-            choices: ['MIT', 'APACHE', 'GNU', 'BSD3','NONE', 'OTHER'],
-         },
+const fs = require("fs")
+const inquirer = require("inquirer")
+const generateMarkdown = require("./utils/generateMarkdown")
 
 
-    ])
-};
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title for this project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is your description for this project?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How does one install this project?"
+    },    
+    {
+        type: "input",
+        name: "usage",
+        message: "How is this project used?"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "How does someone make contributions to the project?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please enter any testing instructions you would like to provide for this project."
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license would you like to use?",
+        choices: ["None", "MIT", "Apache", "BDS 3-Clause", "Mozilla"]
+    },
+    {
+        type: "input",
+        name: "gitHub",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email address?",
+    },
+];
 
-// TODO: Create a function to write README file
-questions()
-  .then((readmeInfo) => {
-    console.log(readmeInfo);
-    return fs.writeFileSync("./readme.md", generateMarkdown.generateMarkdown(readmeInfo));
-  })
-  .catch((err) => {
-    if (err) {
-      throw err;
-    }
-  });
 
-// TODO: Create a function to initialize app
-function writeToFile(filename, readmeInfo) {
-    console.log(filename, readmeInfo);
-    return fs.writeFileSync(path.join(process.cwd(), filename), readmeInfo);
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err){
+        console.log(data)
+        if(err){
+            console.log(err)
+        } else {
+            console.log("You have successfuolly created your readme.")
+        }
+    })
 }
-// Function call to initialize app
+
+function init() {
+    inquirer.prompt(questions)
+    .then(function(data){
+        writeToFile("README.md", generateMarkdown(data))
+        console.log(data)
+    })
+}
+
 init();
--- README.md
+++ README.md
@@ -1,3 +1,37 @@
-# Professional README Generator Starter Code
+# README.md Generator
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+# Description
+This is a node.js application that creates a professional readme.md
+
+# Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+# Installation
+You need to install the following dependencies: Inquirer
+
+# Usage
+To use, clone this repo, run "npm i" and then run "node index.js"
+
+# License 
+This application is covered under the following license:
+
+
+[![License](https://img.shields.io/badge/License-Mozilla_2.0-orange.svg)](https://opensource.org/licenses/MPL-2.0)
+
+
+# Contributing 
+Fork this repo
+
+# Tests
+n/a
+
+# Questions 
+* GitHub Username: https://github.com/simonebrin
+* Please email the following address with any additional questions:
+simonebrin@gmail.com
+
